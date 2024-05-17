4aa34ac91ca2c759575ca23d420498b2b211cc88
leila123myges <119065447+leila123myges@users.noreply.github.com>
2024-05-17 14:44:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+import redis
+from flask import Flask, render_template_string, request
 
 app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        cache.set('data', request.form['data'])
+    data = cache.get('data') or b'Hello, Docker!'    
+
+    return render_template_string('''
+        <form method="post">
+            <input type="text" name="data" value="{{ data.decode('utf-8') }}">
+            <button>Submit</button>
+        </form>
+        <h1>{{ data.decode('utf-8') }}</h1>
+    ''', data=data)
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
-Dans le répertoire : docker build -t flask_app01 .
-docker run -d -p 5000:5000 flask_app01
